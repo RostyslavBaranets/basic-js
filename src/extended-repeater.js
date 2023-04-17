fb41e526d8e22fffeab3459970a143af89229e35
RostyslavBaranets <baranets.01@gmail.com>
2023-04-17 18:53:10 +0300

    d

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( str, options ) {
+  let result = '';
+  
+  if(!options.separator){
+    options.separator = '+';
+  }
+  if(!options.additionSeparator){
+    options.additionSeparator = '|';
+  }
+
+  if(!options.repeatTimes && str){
+    options.repeatTimes = 1;
+  }
+  if(!options.additionRepeatTimes && options.addition){
+    options.additionRepeatTimes = 1;
+  }
+
+    for(let i = 0; i < options.repeatTimes; i++){
+      result += str;
+
+      if(options.additionRepeatTimes){
+        for(let j = 0; j < options.additionRepeatTimes; j++){
+          result += options.addition;
+          if(j !== options.additionRepeatTimes - 1){
+            result += options.additionSeparator;
+          }
+        }
+      }
+
+      if(i !== options.repeatTimes - 1){
+        result += options.separator;
+      }
+    }
+  
+
+  return result;
 }
 
 module.exports = {
